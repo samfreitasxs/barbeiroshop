1c93fe1c6bc2b3423d59ffc4df23d24190d7110e
samfreitasxs <samuel.sfdf@gmail.com>
2024-07-11 22:53:58 -0300

    first commit

---
+++ app/src/main/java/com/samdev/barbeiroshop/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.samdev.barbeiroshop
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.samdev.barbeiroshop.databinding.ActivityMainBinding
+import com.samdev.barbeiroshop.view.Home
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.btLogin.setOnClickListener {
+
+            val nome = binding.editNome.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when{
+                nome.isEmpty() -> {
+                    mensagem(it,"Coloque seu nome!")
+                }senha.isEmpty() -> {
+                    mensagem(it,"Preencha sua senha!")
+                }senha.length <=5 -> {
+                    mensagem(it,"A senha precisa ter pelo menos 6 caracteres!")
+                }else ->{
+                    navegarPraHome(nome)
+                }
+
+            }
+
+        }
+
+    }
+    private fun mensagem(view: View, mensagem: String) {
+        val snackbar = Snackbar.make(view,mensagem,Snackbar.LENGTH_SHORT)
+        snackbar.setBackgroundTint(Color.parseColor("#FF0000"))
+        snackbar.setTextColor(Color.parseColor("FFFFFF"))
+        snackbar.show()
+    }
+
+    private fun navegarPraHome(nome: String){
+        val intent = Intent(this,Home::class.java)
+        intent.putExtra("nome", nome)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/samdev/barbeiroshop/view/Home.kt
@@ -0,0 +1,15 @@
+package com.samdev.barbeiroshop.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.samdev.barbeiroshop.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        }
+    }
